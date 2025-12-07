77ebdb30d3f5b6e200bc8164bd7301c60846a586
Victor <victor.rius@estudiant.enti.cat>
2025-12-07 17:38:39 +0100

    Edición constantes
    Puesta en comun de las constantes entre integrantes del grupo

---
+++ const.py
@@ -0,0 +1,17 @@
+ALFABETO = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+LONGITUD_ALFABETO = 26
+
+# Nombres de archivos
+ROTOR_1 = "Rotor_1.txt"  
+ROTOR_2 = "Rotor_2.txt"
+ROTOR_3 = "Rotor_3.txt"
+MENSAJE_FILE = "Missatge.txt"
+CIFRADO_FILE = "Xifrat.txt"
+DESCIFRADO_FILE = "Desxifrat.txt"
+
+# Configuración por defecto de rotores
+ROTOR_ESTANDAR_CONFIGURACION = [
+    ("FNHCORPGZYWJAMDSLBUVQTKXIE", "P"),  # Rotor 1 
+    ("VLZJMXTBSGEQKIYCROFWHAPNUD", "C"),  # Rotor 2 
+    ("PSWGKQAHUFZORLVETDJYCBMNIX", "G")   # Rotor 3
+]
