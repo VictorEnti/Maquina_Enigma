4d69908e38f02fa3c8add45aef2f2ea9b54bded3
Artur <artur.abrahamyan@estudiant.enti.cat>
2025-12-07 14:27:31 +0100

    Creación Constantes
    Se han creado algunas constantes, faltan por añadir.

---
+++ const.py
@@ -0,0 +1,15 @@
+ALFABETO = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+LONGITUD_ALFABETO = 26
+
+# Nombres de archivos
+ROTOR_FILES = ["Rotor1.txt", "Rotor2.txt", "Rotor3.txt"]
+MENSAJE_FILE = "Missatge.txt"
+CIFRADO_FILE = "Xifrat.txt"
+DESCIFRADO_FILE = "Desxifrat.txt"
+
+# Configuración por defecto de rotores
+ROTOR_ESTANDAR_CONFIGURACION = [
+    ("EKMFLGDQVZNTOWYHXUSPAIBRCJ", "Q"),  # Rotor 1 
+    ("AJDKSIRUXBLHWTMCQGZNPYFVOE", "E"),  # Rotor 2 
+    ("BDFHJLCPRTXVZNYEIWGAKMUSQO", "V")   # Rotor 3 
+]
